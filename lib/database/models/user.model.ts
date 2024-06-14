80f6c70fc0dbfe833a9955027dcd6d707f96cb00
brandon park <brandonpark206@gmail.com>
2024-06-14 14:59:24 -0700

    testing clerk webhook

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,13 +1,32 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true},
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
 });
 
-const User = models.User || model('User', UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
