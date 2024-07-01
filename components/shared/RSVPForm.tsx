36f14ff95cb88f3c2fbdee795a2e23e176cfe6e8
brandon park <brandonpark206@gmail.com>
2024-06-30 22:28:16 -0700

    update rsvp form ui

--- components/shared/RSVPForm.tsx
+++ components/shared/RSVPForm.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import { rsvpFormSchema } from "@/lib/validator"
+import { useEffect } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { rsvpFormSchema } from "@/lib/validator";
 import {
   Form,
   FormControl,
@@ -12,129 +13,76 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Textarea } from "@/components/ui/textarea"
+} from "@/components/ui/form";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Textarea } from "@/components/ui/textarea";
 
 interface RSVPFormProps {
-  userId: string,
-  type: 'Create' | 'Update'
+  userId: string;
+  type: "Create" | "Update";
 }
 
-const DINNER_OPTIONS = ["Steak", "Chicken", "Vegetarian", "NotPicked"] as const;
-const DinnerChoiceEnum = z.enum(DINNER_OPTIONS);
+const DinnerChoiceEnum = {
+  Steak: "Steak",
+  Chicken: "Chicken",
+  Vegetarian: "Vegetarian",
+  NotPicked: "Not Picked",
+};
 
-const RSVPForm = ({userId, type} : RSVPFormProps) => {
+type DinnerChoiceType = "Steak" | "Chicken" | "Vegetarian" | "NotPicked";
+
+const RSVPForm = ({ userId, type }: RSVPFormProps) => {
   const initialValues = {
     isAttending: false,
     numberOfAttendees: undefined,
-    namesOfAttendees: '',
-    dinnerChoice: DinnerChoiceEnum.enum.NotPicked,
-    commentsOrQuestions: ''
+    namesOfAttendees: "",
+    dinnerChoice: DinnerChoiceEnum.NotPicked as DinnerChoiceType,
+    commentsOrQuestions: "",
   };
 
   const form = useForm<z.infer<typeof rsvpFormSchema>>({
     resolver: zodResolver(rsvpFormSchema),
     defaultValues: initialValues,
-  })
+  });
+
+  const { control, handleSubmit, watch, reset } = form;
+
+  const isAttending = watch('isAttending');
+
+  useEffect(() => {
+    if (!isAttending) {
+      reset({
+        namesOfAttendees: '',
+        dinnerChoice: 'NotPicked',
+        commentsOrQuestions: ''
+      });
+    }
+  }, [isAttending, reset]);
 
   function onSubmit(values: z.infer<typeof rsvpFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
         <div className="flex flex-col gap-5 md:flex-row">
           <FormField
-            control={form.control}
+            control={control}
             name="isAttending"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Can You Attend?</FormLabel>
+                <FormLabel>I will be attending!</FormLabel>
                 <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    className="flex flex-col space-y-1"
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="all" />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        Yes!
-                      </FormLabel>
-                    </FormItem>
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="mentions" />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        No, I cannot make it
-                      </FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="flex flex-col gap-5 md:flex-row">
-          <FormField
-            control={form.control}
-            name="dinnerChoice"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Dinner Choice:</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    className="flex flex-col space-y-1"
-                  >
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="all" />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        {DinnerChoiceEnum.enum.Steak}
-                      </FormLabel>
-                    </FormItem>
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="mentions" />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        {DinnerChoiceEnum.enum.Chicken}
-                      </FormLabel>
-                    </FormItem>
-                    <FormItem className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value="none" />
-                      </FormControl>
-                      <FormLabel className="font-normal">{DinnerChoiceEnum.enum.Vegetarian}</FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <div className="flex flex-col gap-5 md:flex-row">
-          <FormField
-            control={form.control}
-            name="namesOfAttendees"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Names of Attendees</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="List out the names of attendees"
-                    className="resize-none"
-                    {...field}
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
                   />
                 </FormControl>
                 <FormMessage />
@@ -142,29 +90,100 @@ const RSVPForm = ({userId, type} : RSVPFormProps) => {
             )}
           />
         </div>
-        <div className="flex flex-col gap-5 md:flex-row">
-          <FormField
-            control={form.control}
-            name="commentsOrQuestions"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Comments or Questions:</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Any questions or comments?"
-                    className="resize-none"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+        {isAttending ? (
+          <>
+            <div className="flex flex-col gap-5 md:flex-row">
+              <FormField
+                control={form.control}
+                name="dinnerChoice"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Dinner Choice:</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        value={field.value}
+                        onValueChange={field.onChange}
+                        className="flex flex-col space-y-1"
+                      >
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value={DinnerChoiceEnum.Steak} />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {DinnerChoiceEnum.Steak}
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value={DinnerChoiceEnum.Chicken} />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {DinnerChoiceEnum.Chicken}
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem
+                              value={DinnerChoiceEnum.Vegetarian}
+                            />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {DinnerChoiceEnum.Vegetarian}
+                          </FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5 md:flex-row">
+              <FormField
+                control={form.control}
+                name="namesOfAttendees"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Names of Attendees</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="List out the names of attendees"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-5 md:flex-row">
+              <FormField
+                control={form.control}
+                name="commentsOrQuestions"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Comments or Questions:</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Any questions or comments?"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </>
+        ) : (
+          ""
+        )}
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default RSVPForm
+export default RSVPForm;
